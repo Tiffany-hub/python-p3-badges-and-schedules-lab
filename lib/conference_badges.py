5028e6ec1b2f406548e1eec6d1704b07c4c5a1d5
+ <+>
2023-08-25 08:30:23 +0300

    completed assignment

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,22 @@
 def badge_maker(name):
-    return None
+    return "Hello, my name is {}.".format(name)
 
 def batch_badge_creator(names):
-    return None
+    badges = [badge_maker(name) for name in names]
+    return badges
 
 def assign_rooms(names):
-    return None
+    room_assignments = ["Hello, {}! You'll be assigned to room {}!".format(name, index + 1) for index, name in enumerate(names)]
+    return room_assignments
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+    
+    for badge in badges:
+        print(badge)
+    
+    for assignment in room_assignments:
+        print(assignment)
+speaker_names = ["Arel", "Carol"]
+printer(speaker_names)
